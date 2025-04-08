7976004f1904fa8d1c371d9458a125d9c8f7c706
Chetan Sutar <chetansutar183@gmail.com>
2025-04-08 16:05:33 +0530

    Add project files.

---
+++ CRUD/Controllers/ProductsController.cs
@@ -0,0 +1,83 @@
+﻿using CRUD.Data;
+using CRUD.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProductsController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Products> products = _context.Products.ToList();
+            return View(products);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create([Bind("Name,Price,Description,Stock")] Products model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Products.Add(model);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var product = _context.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, [Bind("ID,Name,Price,Description,Stock")] Products model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Products.Update(model);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var product = _context.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        public IActionResult DeleteConfirm(int id)
+        {
+            var product = _context.Products.Find(id);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+
+        }
+    }
+}
---
+++ CRUD/Models/Products.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUD.Models
+{
+    public class Products
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+        [Required]
+        public float Price { get; set; }
+        public string Description { get; set; }
+        [Range(0, int.MaxValue,ErrorMessage ="Stock Must Be Positive")]
+        public int  Stock { get; set; }
+    }
+}
